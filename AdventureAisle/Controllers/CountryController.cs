86e03d51979e9860b8bbd42d37401bb8a06444e7
Nikol <482269@student.fontys.nl>
2023-02-28 11:45:28 +0100

    Initial version

---
+++ AdventureAisle/Controllers/CountryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdventureAisle.Controllers
+{
+    public class CountryController : Controller
+    {
+        public IActionResult France()
+        {
+            return View();
+        }
+    }
+}
